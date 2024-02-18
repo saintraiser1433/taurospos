2a17fc3e3ce5a292a2751f4c6991a19294458753
John <decosta.johnrey30@gmail.com>
2024-02-18 17:25:26 +0800

    first commit

---
+++ admin/department.php
@@ -0,0 +1,192 @@
+<?php include '../connection.php';
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+<?php include '../util/head.php' ?>
+
+<body class=" layout-fluid">
+
+  <div class="page">
+    <!-- Navbar -->
+    <?php include '../util/navbar.php' ?>
+    <?php include '../util/sidebar.php' ?>
+    <div class="page-wrapper">
+      <!-- Page header -->
+      <div class="page-header d-print-none">
+        <div class="container-xl">
+          <div class="row g-2 align-items-center">
+            <div class="col">
+              <!-- Page pre-title -->
+              <div class="page-pretitle">
+                Overview
+              </div>
+              <h2 class="page-title">
+                Department
+              </h2>
+            </div>
+            <!-- Page title actions -->
+            <div class="col-auto ms-auto d-print-none">
+              <div class="btn-list">
+                <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
+                  <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 5l0 14" />
+                    <path d="M5 12l14 0" />
+                  </svg>
+                  Create new report
+                </a>
+                <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal" data-bs-target="#modal-report" aria-label="Create new report">
+                  <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 5l0 14" />
+                    <path d="M5 12l14 0" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Page body -->
+      <div class="page-body">
+        <div class="container-xl">
+          <div class="card">
+            <div class="card-body">
+              <div id="listjs">
+                <div class="d-flex align-items-center justify-content-between">
+                  <div></div>
+                  <div class="flex-shrink-0">
+                    <input class="form-control listjs-search" id="search-input" placeholder="Search" style="max-width: 200px;" />
+                  </div>
+                </div>
+                <br>
+                <div id="pagination-container"></div>
+                <div id="table-default" class="table-responsive">
+                  <table class="table" id="tables">
+                    <thead>
+                      <tr>
+                        <th>
+                          <button class="table-sort" data-sort="sort-id">
+                            #
+                          </button>
+                        </th>
+                        <th>
+                          <button class="table-sort" data-sort="sort-department">
+                            Department
+                          </button>
+                        </th>
+                        <th>
+                          <button class="table-sort" data-sort="sort-status">
+                            Status
+                          </button>
+                        </th>
+                        <th>
+                          <button class="table-sort">
+                            Action
+                          </button>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody class="table-tbody">
+                      <?php
+                      $sql = "SELECT * from tbl_department order by department_id asc";
+                      $rs = $conn->query($sql);
+                      $i = 1;
+                      foreach ($rs as $row) { ?>
+                        <tr>
+                          <td class="sort-id"><?php echo $i++ ?></td>
+                          <td class="sort-department text-capitalize"><?php echo $row['department_name'] ?></td>
+                          <td class="sort-status">
+                            <?php
+                            if ($row['status'] == 1) {
+                              echo '<span class="badge badge-sm bg-green-lt text-uppercase ms-auto">Active</span>';
+                            } else if ($row['status'] == 2) {
+                              echo '<span class="badge badge-sm bg-red-lt text-uppercase ms-auto">Inactive</span>';
+                            }
+                            ?>
+
+                          </td>
+                          <td>
+                            <a href="#" class="badge bg-yellow edit">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                                <path d="M6 21v-2a4 4 0 0 1 4 -4h3.5" />
+                                <path d="M18.42 15.61a2.1 2.1 0 0 1 2.97 2.97l-3.39 3.42h-3v-3l3.42 -3.39z" />
+                              </svg>
+
+                            </a> |
+                            <a href="#" class="badge bg-red delete">
+                              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M4 7h16" />
+                                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                <path d="M10 12l4 4m0 -4l-4 4" />
+                              </svg>
+                            </a>
+                          </td>
+                        </tr>
+
+                      <?php } ?>
+
+
+                    </tbody>
+                  </table>
+                  <br>
+                  <div class="btn-toolbar">
+                    <p class="mb-0" id="listjs-showing-items-label">Showing 0 items</p>
+                    <ul class="pagination ms-auto mb-0"></ul>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="footer footer-transparent d-print-none">
+        <div class="container-xl">
+          <div class="row text-center align-items-center flex-row-reverse">
+            <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+              <ul class="list-inline list-inline-dots mb-0">
+                <li class="list-inline-item">
+                  Copyright &copy; 2023
+                  <a href="." class="link-secondary">Tabler</a>.
+                  All rights reserved.
+                </li>
+                <li class="list-inline-item">
+                  <a href="./changelog.html" class="link-secondary" rel="noopener">
+                    v1.0.0-beta20
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+
+  <?php include '../util/script.php' ?>
+
+</body>
+
+</html>
---
+++ util/head.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>SEAIT BORROWING SYSTEM</title>
+    <!-- CSS files -->
+    <link href="../dist/css/tabler.min.css?1692870487" rel="stylesheet"/>
+    <link href="../dist/css/tabler-flags.min.css?1692870487" rel="stylesheet"/>
+    <link href="../dist/css/tabler-payments.min.css?1692870487" rel="stylesheet"/>
+    <link href="../dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet"/>
+    <link href="../dist/css/demo.min.css?1692870487" rel="stylesheet"/>
+    <style>
+      @import url('https://rsms.me/inter/inter.css');
+      :root {
+      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+      }
+      body {
+      	font-feature-settings: "cv03", "cv04", "cv11";
+      }
+    </style>
+    <script src="../dist/js/demo-theme.min.js?1692870487"></script>
+  </head>
+  
